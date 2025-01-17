0063ba60c466fd0d37b4a85baac34d89b267d1d8
Mark Peters <mapeters101@earthlink.net>
2025-01-17 14:58:02 -0500

    Work on lesson017 problem 2b: Problem statement.

---
+++ lesson017b.py
@@ -0,0 +1,9 @@
+# The linear, surface, and volumetric expansions can be calculated,
+# respectively, by the following formulas:
+# delta_linear = length * lin_exp_coef * delta_temp
+# delta_area = area * area_exp_coef * delta_temp
+# delta_volume = volume * vol_exp_coef * delta_temp
+# Write code that displays a menu for the user showing three options: if he/she
+# wants to calculate the linear expansion, he/she must type 1.  if he/she
+# # wants to calculate the surface expansion, he/she must type 2. if he/she
+# # wants to calculate the volumetric expansion, he/she must type 3.
