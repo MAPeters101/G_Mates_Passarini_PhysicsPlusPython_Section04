c3ac1c0a6e804b3dfc91097cddd9d3c60c4dd6c7
Mark Peters <mapeters101@earthlink.net>
2025-01-17 14:46:09 -0500

    Work on lesson017 problem 1: Enter code.

--- lesson017.py
+++ lesson017.py
@@ -2,4 +2,19 @@
 # Find the roots of the equations.
 # The exercise must have three user inputs to determine the equation:
 # a, b, and c.
+import math
+a = float(input("Please enter the value for a: "))
+b = float(input("Please enter the value for b: "))
+c = float(input("Please enter the value for c: "))
 
+delta = b*b - 4*a*c
+
+if delta > 0:
+    root1 = (-b + math.sqrt(delta))/(2*a)
+    root2 = (-b - math.sqrt(delta))/(2*a)
+    print(f"The two roots are: {root1} and {root2}")
+elif delta == 0:
+    root = (-b)/(2*a)
+    print(f"The 1 root is: {root}")
+else:
+    print("There are no real roots.")
