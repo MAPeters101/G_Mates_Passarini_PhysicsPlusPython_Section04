74bb7300c4effffb0bee237aa36be7c348e3ca07
Mark Peters <mapeters101@earthlink.net>
2025-01-17 15:01:59 -0500

    Work on lesson017 problem 2b: Enter code for menu selection.

--- lesson017b.py
+++ lesson017b.py
@@ -5,5 +5,14 @@
 # delta_volume = volume * vol_exp_coef * delta_temp
 # Write code that displays a menu for the user showing three options: if he/she
 # wants to calculate the linear expansion, he/she must type 1.  if he/she
-# # wants to calculate the surface expansion, he/she must type 2. if he/she
-# # wants to calculate the volumetric expansion, he/she must type 3.
+# wants to calculate the surface expansion, he/she must type 2. if he/she
+# wants to calculate the volumetric expansion, he/she must type 3.
+
+print("Please select an option from the following: ")
+print("\tEnter 1 to calculate Linear expansion.")
+print("\tEnter 2 to calculate Surface expansion.")
+print("\tEnter 3 to calculate Volumetric expansion.")
+choice = int(input("Enter selection: "))
+
+
+
